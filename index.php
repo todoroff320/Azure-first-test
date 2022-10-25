5417838935a9c1099d7b6a4c102345739c16d8f0
VICTUS\nikol <s196290@stud.uni-ruse.bg>
2022-10-25 13:08:48 +0300

    2 new files

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        Здравей :)
+        <a href = "contacts.php">Contacts</a>
+    </body>
+</html>
